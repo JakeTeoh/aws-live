bd524aa310b36e284e76483194bb0242d82d1d83
JakeTeoh <70003291+JakeTeoh@users.noreply.github.com>
2023-08-29 16:32:36 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.c7qv5pz2qhty.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "teohxinzhi-employee"
+customregion = "us-east-1"
+
